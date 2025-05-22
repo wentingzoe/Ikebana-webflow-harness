e65f38c909ff3379a186300ba3c9a51df30ee13f
wentingzoe <ywtzoe@gmail.com>
2025-05-22 12:00:00 +0000

    chore: add typescript eslint prettier config

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  env: {
+    es2021: true,
+    node: true
+  },
+  ignorePatterns: ['dist/', 'node_modules/'],
+  plugins: ['@typescript-eslint', 'import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier'
+  ],
+  rules: {
+    'import/order': [
+      'warn',
+      {
+        groups: [['builtin', 'external'], 'internal', ['parent', 'sibling', 'index']],
+        'newlines-between': 'always'
+      }
+    ]
+  }
+};
